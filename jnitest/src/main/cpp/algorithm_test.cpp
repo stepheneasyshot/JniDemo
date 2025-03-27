b884893321790a781952c402407cfaf0b9c3c61a
stephenzhan <zhanfeng990927@gmail.com>
2025-03-27 17:04:10 +0800

    Update algorithm_test.cpp

--- jnitest/src/main/cpp/algorithm_test.cpp
+++ jnitest/src/main/cpp/algorithm_test.cpp
@@ -1,6 +1,7 @@
 #include <jni.h>
 #include <string>
 #include <android/log.h>
+#include <vector>
 
 // Android log function wrappers
 static const char *kTAG = "ALGORITHM_TEST";
@@ -16,7 +17,14 @@ extern "C" JNIEXPORT void JNICALL
 Java_com_stephen_jnitest_AlgorithmDemo_combineTwoArrays(JNIEnv *env, jobject) {
     LOGI("Java_com_stephen_jnitest_AlgorithmDemo_combineTwoArrays triggered");
     int a[] = {1, 2, 3, 4, 5};
-    int b[] = {0,0,0};
+    int b[] = {0, 0, 0};
+    int m = sizeof(a) / sizeof(a[0]);
+    int n = sizeof(b) / sizeof(b[0]);
+    int arrayCombined[m + n];
+    for (int i = 0; i < m; i++) {
+        arrayCombined[i] = a[i];
+    }
+
 
 
 }
